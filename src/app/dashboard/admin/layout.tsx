e5d3092a4999e337dc3a8dcc3d4d9ff97252f501
Nurpeisdev <nurpeisdev@gmail.com>
2025-02-19 15:29:38 +0500

    Creating an Efficient Admin Dashboard Header

--- src/app/dashboard/admin/layout.tsx
+++ src/app/dashboard/admin/layout.tsx
@@ -1,7 +1,25 @@
-export default function AdminDashboardLayout({
+import Header from "@/components/dashboard/header/header";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+export default async function AdminDashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <div>{children}</div>;
+  // Block non admins accessing the admin dashboard
+  const user = await currentUser();
+  if (!user || user?.privateMetadata.role !== "ADMIN") {
+    redirect("/");
+  }
+  return (
+    <div className="w-full h-full">
+      {/* Sidebar */}
+      <div className="w-full ml-[300px]">
+        {/* Header */}
+        <Header />
+        <div className="w-full mt-[75px] p-4">{children}</div>
+      </div>
+    </div>
+  );
 }
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,16 @@
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 import React from "react";
 
 // Get user and redirect depending on role
 
-export default function DashboardPage() {
+export default async function DashboardPage() {
+  // Get user and redirect depending on role
+  const user = await currentUser();
+  if (!user?.privateMetadata?.role || user?.privateMetadata.role === "USER")
+    redirect("/");
+
+  if (user.privateMetadata.role === "ADMIN") redirect("/dashboard/admin");
+  if (user.privateMetadata.role === "SELLER") redirect("/dashboard/seller");
   return <div>Dashboard page</div>;
 }
